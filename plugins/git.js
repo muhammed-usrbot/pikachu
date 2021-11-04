27803267b3819bb216c2a9130e86b36ec200d2b9
muhammed-usrbot <93637844+muhammed-usrbot@users.noreply.github.com>
2021-11-04 16:50:46 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,7 +7,7 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://www.linkpicture.com/q/TAURUSGFX.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*TAURUS BOT BY MUHAMMED*
 
