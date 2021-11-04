6b6a2d6824c0cad26d565485ade8de8089e1e228
muhammed-usrbot <93637844+muhammed-usrbot@users.noreply.github.com>
2021-11-04 17:11:08 +0530

    Update aveng.js

--- plugins/aveng.js
+++ plugins/aveng.js
@@ -1,6 +1,7 @@
-/*coded by ameer&hisham
+/*coded by muhammed&hisham
 youtube https://youtube.com/c/ICHUTECH
 https://youtube.com/channel/UCpGa88rhUFYj-6-LBqBbWKw
+https://youtube.com/channel/UCeYZqtAtdYq8VwSIkW34JMA
 */
 
 const Asena = require('../events');
@@ -18,7 +19,7 @@ if (Config.WORKTYPE == 'private') {
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/logoaveng?apikey=Upe1Fp1lDAtX0ioPYLEPsSoX51i&text1=Pikachu&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by PIKACHU' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by TAURUS' })
 
     }));
 }
@@ -32,7 +33,7 @@ else if (Config.WORKTYPE == 'public') {
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/logoaveng?apikey=Upe1Fp1lDAtX0ioPYLEPsSoX51i&text1=Pikachu&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by PIKACHU' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by TAURUS' })
 
     }));
     
